51c77c06449467a3149d14958da7ca150425733d
Note-KaTSuo <mwalisom9@gmail.com>
2023-02-28 20:57:42 -0300

    Add

---
+++ ExerciciosJava/src/Exercicios/Ex01.java
@@ -0,0 +1,6 @@
+package Exercicios;
+public class Ex01 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá mundo...");
+    }
+}
---
+++ ExerciciosJava/src/Exercicios/Ex02.java
@@ -0,0 +1,21 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        int valor1;
+        int valor2;
+
+        Scanner sc = new Scanner(System.in);
+
+        valor1 = sc.nextInt();
+        valor2 = sc.nextInt();
+
+        if (valor1 > valor2) {
+            System.out.println("O primeiro valor é maior");
+        }
+
+
+    }
+}
