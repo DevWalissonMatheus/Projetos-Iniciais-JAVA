f398dabddd5d4cbedbaf419e27ba86006255c3c3
Note-KaTSuo <mwalisom9@gmail.com>
2023-03-01 19:16:38 -0300

    Adicionando um else para verificar se o segundo valor é maior

--- ExerciciosJava/src/Exercicios/Ex03.java
+++ ExerciciosJava/src/Exercicios/Ex03.java
@@ -1,7 +1,34 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 public class Ex03 {
     public static void main(String[] args) {
-        
+        int idade1=0;
+        int idade2=0;
+
+        int dif1;
+        int dif2;
+
+        Scanner idd1 = new Scanner(System.in);
+        System.out.print("Insira a primeira idade: ");
+        idade1 = idd1.nextInt();
+
+        Scanner idd2 = new Scanner(System.in);
+        System.out.print("Insira a segunda idade: ");
+        idade2 = idd2.nextInt();
+
+        dif1 = idade1 - idade2;
+        dif2 = idade2 - idade1;
+
+        if (idade1 > idade2) {
+            System.out.println("A diferença é de " + dif1 + " anos");
+        } else if (idade2 > idade1) {
+            System.out.println("A diferença é de " + dif2 + " anos");
+        }
+
+
+    idd1.close();
+    idd2.close();
     }
 }
