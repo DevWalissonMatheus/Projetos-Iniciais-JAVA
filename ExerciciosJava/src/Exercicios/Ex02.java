09f1496e4b499348fd6ad9732ad80d819b033208
Note-KaTSuo <mwalisom9@gmail.com>
2023-03-01 14:24:02 -0300

    Adicionando a 3 classe de exercicio e trocando println por print para que a pergunta seja feita na frente da pergunta e não abaixo

--- ExerciciosJava/src/Exercicios/Ex02.java
+++ ExerciciosJava/src/Exercicios/Ex02.java
@@ -8,23 +8,21 @@ public class Ex02 {
         int valor2;
 
         Scanner sc1 = new Scanner(System.in);
-        System.out.println("Insira o primeiro número: ");
+        System.out.print("Insira o primeiro número: ");
         valor1 = sc1.nextInt();
 
         Scanner sc2 = new Scanner(System.in);
-        System.out.println("Insira o segundo número: ");
+        System.out.print("Insira o segundo número: ");
         valor2 = sc2.nextInt();
 
         if (valor1 > valor2) {
-            System.out.println("O primeiro valor é maior!");
+            System.out.println("O primeiro número é maior!");
         } else {
-            System.out.println("O segundo valor é maior!");
+            System.out.println("O segundo número é maior!");
         }
-
 
     sc1.close();
     sc2.close();
-
 
     }
 }
---
+++ ExerciciosJava/src/Exercicios/Ex03.java
@@ -0,0 +1,7 @@
+package Exercicios;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        
+    }
+}
